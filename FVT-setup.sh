1e0a75ae9330ac853cbbea6f32332658a9de4e26
Bradley Allen <allen.bradley@ymail.com>
2019-08-16 14:36:00 -0500

    hooks/FVT-setup.sh added SAST test cases #26

--- FVT-setup.sh
+++ FVT-setup.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-# 	hooks/FVT-setup.sh  3.247.474  2019-08-11T07:51:53.596729-05:00 (CDT)  https://github.com/BradleyA/user-files.git  uadmin  one-rpi3b.cptx86.com 3.246-2-g0eac612  
-# 	   change test case output extension from .out to .test-case-output #26 
-# 	hooks/FVT-setup.sh  3.246.471  2019-08-10T20:05:23.559251-05:00 (CDT)  https://github.com/BradleyA/user-files.git  uadmin  one-rpi3b.cptx86.com 3.245-1-g58d7e0e  
-# 	   ### production standard 10.0 FVT testing #26 
-# 	hooks/FVT-setup.sh  3.244.468  2019-08-10T19:47:00.778853-05:00 (CDT)  https://github.com/BradleyA/user-files.git  uadmin  one-rpi3b.cptx86.com 3.243  
-# 	   hooks/FVT-setup.sh hooks/post-commit move rm -f FVT-*.out from hooks/post-commit to hooks/FVT-setup.sh 
-# 	hooks/FVT-setup.sh  3.243.467  2019-08-10T19:39:36.382334-05:00 (CDT)  https://github.com/BradleyA/user-files.git  uadmin  one-rpi3b.cptx86.com 3.242  
-# 	   hooks/FVT-setup.sh example FVT-setup.sh file to be placed in TEST/<file_name>/ directory with test cases 
+# 	hooks/FVT-setup.sh  3.264.508  2019-08-16T14:35:19.798883-05:00 (CDT)  https://github.com/BradleyA/user-files.git  uadmin  one-rpi3b.cptx86.com 3.263  
+# 	   hooks/FVT-setup.sh added SAST test cases #26 
+# 	hooks/FVT-setup.sh  3.264.508  2019-08-16T14:34:43.536825-05:00 (CDT)  https://github.com/BradleyA/user-files.git  uadmin  one-rpi3b.cptx86.com 3.263  
+# 	   hooks/FVT-setup.sh added SAST test cases 
+# 	template/TEST/template.sh/FVT-setup.sh  3.263.507  2019-08-16T14:09:18.293030-05:00 (CDT)  https://github.com/BradleyA/user-files.git  uadmin  one-rpi3b.cptx86.com 3.262-2-gc1be0e1  
+# 	   hooks/post-commit add SAST testing #26 
 ###
 #	FVT-setup.sh -  This script is optional.  It runs before test cases are run, if FVT-setup.sh is found in TEST/<file_name>/
+#	   Functional Verification Testing (FVT) - verify that the program logic conforms to design specification.
+#	   FVT attempts to answer the question "Does this program logic do what is intended?"
 #
-### production standard 3.0 shellcheck
-### production standard 5.1.160 Copyright
+#### production standard 3.0 shellcheck
+#### production standard 5.1.160 Copyright
 ##       Copyright (c) 2019 Bradley Allen
 ##       MIT License is in the online DOCUMENTATION, DOCUMENTATION URL defined below.
-### production standard 10.0 FVT testing
+###
 #       Remove output from previous run of test cases
 rm -f FVT-*.test-case-output
 #	Create symbolic link for all test case expected output that matches another test case expected output 
