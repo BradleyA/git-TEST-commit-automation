5a38bb81995dad8d7027312644f1faff888c943f
Bradley Allen <allen.bradley@ymail.com>
2019-08-17 10:17:44 -0500

    FVT-option-help-001 added SAST test cases

--- FVT-setup.sh
+++ FVT-setup.sh
@@ -1,2 +1,2 @@
-# 	hooks/FVT-setup.sh  3.312.541  2019-08-17T09:46:00.767268-05:00 (CDT)  https://github.com/BradleyA/user-files.git  uadmin  one-rpi3b.cptx86.com 3.311  
-# 	   hooks/FVT-setup.sh moved into EXAMPLES directory 
+# 	hooks/FVT-setup.sh  3.313.544  2019-08-17T10:17:44.226605-05:00 (CDT)  https://github.com/BradleyA/user-files.git  uadmin  one-rpi3b.cptx86.com 3.312-2-g7441282  
+# 	   FVT-option-help-001 added SAST test cases 
