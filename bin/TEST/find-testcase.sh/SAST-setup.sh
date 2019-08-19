9d17a8fad26b2811bc8fec456c093185ebf51ba5
Bradley Allen <allen.bradley@ymail.com>
2019-08-19 14:34:23 -0500

    hooks/bin/TEST/find-testcase.sh/SAST-setup.sh copied to .

--- bin/TEST/find-testcase.sh/SAST-setup.sh
+++ bin/TEST/find-testcase.sh/SAST-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# 	hooks/TEST/find-testcase.sh/SAST-setup.sh  3.309.538  2019-08-17T09:09:35.635217-05:00 (CDT)  https://github.com/BradleyA/user-files.git  uadmin  one-rpi3b.cptx86.com 3.308  
-# 	   hooks/find-testcase.sh added SAST test cases #26 
+# 	hooks/bin/TEST/find-testcase.sh/SAST-setup.sh  3.342.581  2019-08-19T14:34:23.248874-05:00 (CDT)  https://github.com/BradleyA/user-files.git  uadmin  one-rpi3b.cptx86.com 3.341  
+# 	   hooks/bin/TEST/find-testcase.sh/SAST-setup.sh copied to . 
 ###
 #	SAST-setup.sh -  This script is optional.  It runs before test cases are run, if SAST-setup.sh is found in TEST/<file_name>/
 #	   Static Analysis Software Testing (SAST) - examination of the code prior to the program’s execution
@@ -13,4 +13,14 @@
 ###
 #       Remove output from previous run of test cases
 rm -f SAST-*.test-case-output
+#
+### production standard 10.0 TESTing 
+REPOSITORY_DIR=$(git rev-parse --show-toplevel)
+#       Add shared TEST cases with command
+#          uncomment the test case to be tested
+#       SAST
+#       ln -fs ${REPOSITORY_DIR}/hooks/EXAMPLES/SAST-permission-644-001 SAST-permission-644-001
+#       ln -fs ${REPOSITORY_DIR}/hooks/EXAMPLES/SAST-permission-755-001 SAST-permission-755-001
+ln -fs ${REPOSITORY_DIR}/hooks/EXAMPLES/SAST-permission-775-001 SAST-permission-775-001
+ln -fs ${REPOSITORY_DIR}/hooks/EXAMPLES/SAST-shellcheck-001 SAST-shellcheck-001
 ###		
