c30130035072f1aa06f7fa56dca57f0d485f8bb6
Bradley Allen <allen.bradley@ymail.com>
2019-08-19 14:15:31 -0500

    hooks/bin/setup-TEST-cases.sh hooks/bin/teardown-TEST-cases.sh initial commit

---
+++ bin/setup-TEST-cases.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# 	hooks/bin/setup-TEST-cases.sh  3.339.578  2019-08-19T14:15:31.400490-05:00 (CDT)  https://github.com/BradleyA/user-files.git  uadmin  one-rpi3b.cptx86.com 3.338  
+# 	   hooks/bin/setup-TEST-cases.sh hooks/bin/teardown-TEST-cases.sh initial commit 
+###
--- find-testcase.sh
+++
@@ -1,2 +0,0 @@
-# 	hooks/find-testcase.sh  3.338.577  2019-08-19T14:09:32.236186-05:00 (CDT)  https://github.com/BradleyA/user-files.git  uadmin  one-rpi3b.cptx86.com 3.337  
-# 	   hooks/post-commit improve user help explaining why 
