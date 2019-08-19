d44799f848b2cc3f745003445736f8beab0bad43
Bradley Allen <allen.bradley@ymail.com>
2019-08-19 16:25:25 -0500

    testing

--- bin/find-TEST-cases.sh
+++ bin/find-TEST-cases.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# 	hooks/bin/find-TEST-cases.sh  3.353.592  2019-08-19T16:18:40.332366-05:00 (CDT)  https://github.com/BradleyA/user-files.git  uadmin  one-rpi3b.cptx86.com 3.352  
+# 	hooks/bin/find-TEST-cases.sh  3.354.593  2019-08-19T16:25:20.854090-05:00 (CDT)  https://github.com/BradleyA/user-files.git  uadmin  one-rpi3b.cptx86.com 3.353  
 # 	   testing 
 ###
 #	find-testcase.sh -  search from top of repository to find TEST directory and test cases
