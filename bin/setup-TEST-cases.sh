c457591a6f57d2f5a9fa1f73f2285cc485a214ad
Bradley Allen <allen.bradley@ymail.com>
2019-08-19 14:22:23 -0500

    hooks/bin/setup-TEST-cases.sh hooks/bin/teardown-TEST-cases.sh changed file permission

--- bin/setup-TEST-cases.sh
+++ bin/setup-TEST-cases.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# 	hooks/bin/setup-TEST-cases.sh  3.340.579  2019-08-19T14:22:23.150084-05:00 (CDT)  https://github.com/BradleyA/user-files.git  uadmin  one-rpi3b.cptx86.com 3.339  
+# 	   hooks/bin/setup-TEST-cases.sh hooks/bin/teardown-TEST-cases.sh changed file permission 
 # 	hooks/bin/setup-TEST-cases.sh  3.339.578  2019-08-19T14:15:31.400490-05:00 (CDT)  https://github.com/BradleyA/user-files.git  uadmin  one-rpi3b.cptx86.com 3.338  
 # 	   hooks/bin/setup-TEST-cases.sh hooks/bin/teardown-TEST-cases.sh initial commit 
 ###
