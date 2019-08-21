e239ae2b300d5d177e6420edbb6ab68a739c2a49
Bradley Allen <allen.bradley@ymail.com>
2019-08-21 14:27:30 -0500

    hooks/EXAMPLES/FVT-setup.sh  updated syntax of ln to match SAST ; need to begin testing FVT-*

--- EXAMPLES/FVT-setup.sh
+++ EXAMPLES/FVT-setup.sh
@@ -1,51 +1,49 @@
 #!/bin/bash
-# 	hooks/EXAMPLES/FVT-setup.sh  3.335.573  2019-08-18T21:25:54.808725-05:00 (CDT)  https://github.com/BradleyA/user-files.git  uadmin  one-rpi3b.cptx86.com 3.334-1-gb85dde4  
-# 	   hooks/EXAMPLES/FVT-setup.sh hooks/EXAMPLES/SAST-setup.sh add commond test cases with ln 
-# 	hooks/EXAMPLES/FVT-setup.sh  3.313.542  2019-08-17T09:52:21.914768-05:00 (CDT)  https://github.com/BradleyA/user-files.git  uadmin  one-rpi3b.cptx86.com 3.312  
-# 	   moved into EXAMPLES directory 
+# 	hooks/EXAMPLES/FVT-setup.sh  3.424.665  2019-08-21T14:27:30.123978-05:00 (CDT)  https://github.com/BradleyA/user-files.git  uadmin  one-rpi3b.cptx86.com 3.423  
+# 	   hooks/EXAMPLES/FVT-setup.sh  updated syntax of ln to match SAST ; need to begin testing FVT-* 
 ###
 #	FVT-setup.sh -  This script is optional.  It runs before test cases are run, if FVT-setup.sh is found in TEST/<file_name>/
 #	   Functional Verification Testing (FVT) - verify that the program logic conforms to design specification.
 #	   FVT attempts to answer the question "Does this program logic do what is intended?"
-#
 #### production standard 3.0 shellcheck
 #### production standard 5.1.160 Copyright
 ##       Copyright (c) 2019 Bradley Allen
 ##       MIT License is in the online DOCUMENTATION, DOCUMENTATION URL defined below.
-###
+### production standard 1.0 DEBUG variable
+#       Order of precedence: environment variable, default code
+if [ "${DEBUG}" == "" ] ; then DEBUG="0" ; fi   # 0 = debug off, 1 = debug on, 'export DEBUG=1', 'unset DEBUG' to unset environment variable (bash)
+BOLD=$(tput -Txterm bold)
+NORMAL=$(tput -Txterm sgr0)
+#       Date and time function ISO 8601
+get_date_stamp() {
+DATE_STAMP=$(date +%Y-%m-%dT%H:%M:%S.%6N%:z)
+TEMP=$(date +%Z)
+DATE_STAMP="${DATE_STAMP} (${TEMP})"
+}
+#       Fully qualified domain name FQDN hostname
+LOCALHOST=$(hostname -f)
+#       Version
+SCRIPT_VERSION=$(head -2 "${0}" | awk '{printf $3}')
+
+### production standard 10.0 TESTing
 #       Remove output from previous run of test cases
 rm -f FVT-*.test-case-output
-#
-### production standard 10.0 TESTing 
-REPOSITORY_DIR=$(git rev-parse --show-toplevel)
-#	Add shared TEST cases with command
-#	   uncomment the test case to be tested
-#	FVT
-#	ln -fs ${REPOSITORY_DIR}/hooks/EXAMPLES/FVT-option-help-001 ./FVT-option-help-001
-#	ln -fs ${REPOSITORY_DIR}/hooks/EXAMPLES/FVT-option-help-001 ./FVT-option-help-001
-#	ln -fs ${REPOSITORY_DIR}/hooks/EXAMPLES/FVT-option-help-001 ./FVT-option-help-002
-#	ln -fs ${REPOSITORY_DIR}/hooks/EXAMPLES/FVT-option-help-001 ./FVT-option-help-003
-#	ln -fs ${REPOSITORY_DIR}/hooks/EXAMPLES/FVT-option-help-001 ./FVT-option-help-004
-#	ln -fs ${REPOSITORY_DIR}/hooks/EXAMPLES/FVT-option-help-001 ./FVT-option-help-005
-#	ln -fs ${REPOSITORY_DIR}/hooks/EXAMPLES/FVT-option-help-001 ./FVT-option-help-006
-#	ln -fs ${REPOSITORY_DIR}/hooks/EXAMPLES/FVT-option-help-001 ./FVT-option-usage-001
-#	ln -fs ${REPOSITORY_DIR}/hooks/EXAMPLES/FVT-option-help-001 ./FVT-option-usage-002
-#	ln -fs ${REPOSITORY_DIR}/hooks/EXAMPLES/FVT-option-help-001 ./FVT-option-usage-003
-#	ln -fs ${REPOSITORY_DIR}/hooks/EXAMPLES/FVT-option-help-001 ./FVT-option-version-001
-#	ln -fs ${REPOSITORY_DIR}/hooks/EXAMPLES/FVT-option-help-001 ./FVT-option-version-002
-#	ln -fs ${REPOSITORY_DIR}/hooks/EXAMPLES/FVT-option-help-001 ./FVT-option-version-003
 
-#	Create symbolic link for all test case expected output that matches another test case expected output 
-#	Expected output from FVT-option-help-001 matches output from FVT-option-help-00[2,3,4,5,6]
-ln -fs FVT-option-help-001.expected FVT-option-help-002.expected
-ln -fs FVT-option-help-001.expected FVT-option-help-003.expected
-ln -fs FVT-option-help-001.expected FVT-option-help-004.expected
-ln -fs FVT-option-help-001.expected FVT-option-help-005.expected
-ln -fs FVT-option-help-001.expected FVT-option-help-006.expected
-#	Expected output from FVT-option-usage-001 matches output from FVT-option-usage-00[2,3]
-ln -fs FVT-option-usage-001.expected FVT-option-usage-002.expected
-ln -fs FVT-option-usage-001.expected FVT-option-usage-003.expected 
-#	Expected output from FVT-option-version-001 matches output from FVT-option-version-00[2,3]
-ln -fs FVT-option-version-001.expected FVT-option-version-002.expected
-ln -fs FVT-option-version-001.expected FVT-option-version-003.expected
-###		
+REPOSITORY_DIR=${1}
+
+#       uncomment shared TEST cases for command
+ln -fs "${REPOSITORY_DIR}/hooks/EXAMPLES/FVT-option-help-001" FVT-option-help-001
+#	ln -fs "${REPOSITORY_DIR}/hooks/EXAMPLES/FVT-option-help-002" FVT-option-help-002
+#	ln -fs "${REPOSITORY_DIR}/hooks/EXAMPLES/FVT-option-help-003" FVT-option-help-003
+#	ln -fs "${REPOSITORY_DIR}/hooks/EXAMPLES/FVT-option-help-004" FVT-option-help-004
+#	ln -fs "${REPOSITORY_DIR}/hooks/EXAMPLES/FVT-option-help-005" FVT-option-help-005
+#	ln -fs "${REPOSITORY_DIR}/hooks/EXAMPLES/FVT-option-help-006" FVT-option-help-006
+#	ln -fs "${REPOSITORY_DIR}/hooks/EXAMPLES/FVT-option-usage-001" FVT-option-usage-001
+#	ln -fs "${REPOSITORY_DIR}/hooks/EXAMPLES/FVT-option-usage-002" FVT-option-usage-002
+#	ln -fs "${REPOSITORY_DIR}/hooks/EXAMPLES/FVT-option-usage-003" FVT-option-usage-003
+#	ln -fs "${REPOSITORY_DIR}/hooks/EXAMPLES/FVT-option-version-001" FVT-option-version-001
+#	ln -fs "${REPOSITORY_DIR}/hooks/EXAMPLES/FVT-option-version-002" FVT-option-version-002
+#	ln -fs "${REPOSITORY_DIR}/hooks/EXAMPLES/FVT-option-version-003" FVT-option-version-003
+#
+if [ "${DEBUG}" == "1" ] ; then get_date_stamp ; echo -e "${NORMAL}${DATE_STAMP} ${LOCALHOST} ${0}[$$] ${SCRIPT_VERSION} ${LINENO} ${USER} ${USER_ID}:${GROUP_ID} ${BOLD}[DEBUG]${NORMAL}  Operation finished." 1>&2 ; fi
+###
