0240acade3e6173c4d0f79b1723930f5133b4478
Bradley Allen <allen.bradley@ymail.com>
2019-08-22 11:16:38 -0500

    hooks/bin/find-git-TEST-cases.sh  renamed find-TEST-cases.sh -> find-git-TEST-cases.sh

--- bin/teardown-TEST-cases.sh
+++ bin/teardown-TEST-cases.sh
@@ -1,6 +1,2 @@
-#!/bin/bash
-# 	hooks/bin/teardown-TEST-cases.sh  3.340.579  2019-08-19T14:22:23.253592-05:00 (CDT)  https://github.com/BradleyA/user-files.git  uadmin  one-rpi3b.cptx86.com 3.339  
-# 	   hooks/bin/setup-TEST-cases.sh hooks/bin/teardown-TEST-cases.sh changed file permission 
-# 	hooks/bin/teardown-TEST-cases.sh  3.339.578  2019-08-19T14:15:31.503381-05:00 (CDT)  https://github.com/BradleyA/user-files.git  uadmin  one-rpi3b.cptx86.com 3.338  
-# 	   hooks/bin/setup-TEST-cases.sh hooks/bin/teardown-TEST-cases.sh initial commit 
-###
+# 	hooks/bin/teardown-TEST-cases.sh  3.431.677  2019-08-22T11:16:38.741258-05:00 (CDT)  https://github.com/BradleyA/user-files.git  uadmin  one-rpi3b.cptx86.com 3.430-4-g1d9b96f  
+# 	   hooks/bin/find-git-TEST-cases.sh  renamed find-TEST-cases.sh -> find-git-TEST-cases.sh 
