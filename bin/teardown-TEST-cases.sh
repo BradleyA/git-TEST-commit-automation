dd1179e6eab0f97b91aa888602c7844329a5c89b
Bradley Allen <allen.bradley@ymail.com>
2019-08-22 11:18:31 -0500

    delete old file names

--- bin/teardown-TEST-cases.sh
+++
@@ -1,2 +0,0 @@
-# 	hooks/bin/teardown-TEST-cases.sh  3.431.677  2019-08-22T11:16:38.741258-05:00 (CDT)  https://github.com/BradleyA/user-files.git  uadmin  one-rpi3b.cptx86.com 3.430-4-g1d9b96f  
-# 	   hooks/bin/find-git-TEST-cases.sh  renamed find-TEST-cases.sh -> find-git-TEST-cases.sh 
