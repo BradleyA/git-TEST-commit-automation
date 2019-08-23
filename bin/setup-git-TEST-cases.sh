7efcede373caae3bae567be0356f41cc07da2795
Bradley Allen <allen.bradley@ymail.com>
2019-08-22 20:35:19 -0500

    hooks/bin/find-git-TEST-cases.sh  add color to output

--- bin/setup-git-TEST-cases.sh
+++ bin/setup-git-TEST-cases.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
+# 	hooks/bin/setup-git-TEST-cases.sh  3.432.679  2019-08-22T20:35:19.791238-05:00 (CDT)  https://github.com/BradleyA/user-files.git  uadmin  one-rpi3b.cptx86.com 3.431-1-gdfe8546  
+# 	   hooks/bin/find-git-TEST-cases.sh  add color to output 
 # 	hooks/bin/setup-TEST-cases.sh  3.340.579  2019-08-19T14:22:23.150084-05:00 (CDT)  https://github.com/BradleyA/user-files.git  uadmin  one-rpi3b.cptx86.com 3.339  
 # 	   hooks/bin/setup-TEST-cases.sh hooks/bin/teardown-TEST-cases.sh changed file permission 
 # 	hooks/bin/setup-TEST-cases.sh  3.339.578  2019-08-19T14:15:31.400490-05:00 (CDT)  https://github.com/BradleyA/user-files.git  uadmin  one-rpi3b.cptx86.com 3.338  
 # 	   hooks/bin/setup-TEST-cases.sh hooks/bin/teardown-TEST-cases.sh initial commit 
 ###
+#	after clone  repository
+#	copy commands to /usr/local/bin
+#	
+#	need to determine where to start after copying commands
+#
+#
+#
+#
