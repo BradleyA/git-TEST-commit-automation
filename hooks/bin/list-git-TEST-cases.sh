9568dd7a9408fbb953758a5f1ddae7d64b5a8c60
Bradley Allen <allen.bradley@ymail.com>
2019-09-01 18:14:04 -0500

    rename and delete 3 files

--- hooks/bin/list-git-TEST-cases.sh
+++
@@ -1,2 +0,0 @@
-# 	hooks/bin/list-git-TEST-cases.sh  2.6.283  2019-09-01T18:11:13.287566-05:00 (CDT)  https://github.com/BradleyA/git-TEST-commit-automation.git  uadmin  five-rpi3b.cptx86.com 2.5-1-gadc451e  
-# 	   hooks/bin/list-git-TEST-cases.sh  add support to execute clean if found in TEST/<command>/ 
