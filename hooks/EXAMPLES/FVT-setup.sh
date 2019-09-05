a8003e3b4f55e325a0453f2d3552420a4c6ce8fa
Bradley Allen <allen.bradley@ymail.com>
2019-09-05 15:10:32 -0500

    close #11 hooks/EXAMPLES/FVT-setup.sh  upgraded Production standard 1.3.496 DEBUG variable ; added REPOSITORY_DIR for #11 ; added more ln

--- hooks/EXAMPLES/FVT-setup.sh
+++ hooks/EXAMPLES/FVT-setup.sh
@@ -1,24 +1,20 @@
 #!/bin/bash
-# 	hooks/EXAMPLES/FVT-setup.sh  3.488.743  2019-08-31T23:13:27.607919-05:00 (CDT)  https://github.com/BradleyA/user-files.git  uadmin  one-rpi3b.cptx86.com 3.487  
-# 	   SCRIPT_VERSION incident corrected and correct Test script logic ERROR message 
-# 	hooks/EXAMPLES/FVT-setup.sh  3.488.743  2019-08-31T23:11:17.014756-05:00 (CDT)  https://github.com/BradleyA/user-files.git  uadmin  one-rpi3b.cptx86.com 3.487  
-# 	   SCRIPT_VERSION incident corrected and correct Test script logic ERROR message 
-# 	hooks/EXAMPLES/FVT-setup.sh  3.485.740  2019-08-31T21:19:16.394013-05:00 (CDT)  https://github.com/BradleyA/user-files.git  uadmin  one-rpi3b.cptx86.com 3.484  
-# 	   hooks/EXAMPLES/FVT-setup.sh   few typos 
-###  hooks/EXAMPLES/FVT-setup.sh - This script is optional.  It runs before test cases
-#    are run, if FVT-setup.sh is found in TEST/<file_name>/
-#    Functional Verification Testing (FVT) - verify that the program logic conforms to
-#    design specification.  FVT attempts to answer the question "Does this program logic
-#    do what is intended?"
+# 	hooks/EXAMPLES/FVT-setup.sh  2.40.365  2019-09-05T15:10:32.849003-05:00 (CDT)  https://github.com/BradleyA/git-TEST-commit-automation.git  uadmin  five-rpi3b.cptx86.com 2.39-41-g76a187e  
+# 	   close #11 hooks/EXAMPLES/FVT-setup.sh  upgraded Production standard 1.3.496 DEBUG variable ; added REPOSITORY_DIR for #11 ; added more ln 
+#86# hooks/EXAMPLES/FVT-setup.sh - This script is optional.  It is for setting up
+#    'Production standard 10.0 TESTing' test cases.  Copy it to TEST/<file_name>/.
+#    Functional Verification Testing (FVT) - verify that the program logic conforms
+#    to design specification.
 ###  Production standard 3.0 shellcheck
 ###  Production standard 5.1.160 Copyright
 #    Copyright (c) 2019 Bradley Allen
 #    MIT License is in the online DOCUMENTATION, DOCUMENTATION URL defined below.
-###  Production standard 1.3.475 DEBUG variable
+###  Production standard 1.3.496 DEBUG variable
 #    Order of precedence: environment variable, default code
 if [[ "${DEBUG}" == ""  ]] ; then DEBUG="0" ; fi   # 0 = debug off, 1 = debug on, 'export DEBUG=1', 'unset DEBUG' to unset environment variable (bash)
 if [[ "${DEBUG}" == "2" ]] ; then set -x    ; fi   # Print trace of simple commands before they are executed
 if [[ "${DEBUG}" == "3" ]] ; then set -v    ; fi   # Print shell input lines as they are read
+if [[ "${DEBUG}" == "4" ]] ; then set -e    ; fi   # Exit command has a non-zero exit status
 #
 BOLD=$(tput -Txterm bold)
 NORMAL=$(tput -Txterm sgr0)
@@ -42,10 +38,17 @@ if [[ "${DEBUG}" == "1" ]] ; then get_date_stamp ; echo -e "${NORMAL}${DATE_STAM
 #    Remove output from previous run of test cases
 rm -f FVT-*.test-case-output
 
-REPOSITORY_DIR=${1}
+if [[ ! -z "${1}" ]] ; then  # post-commit must pass REPOSITORY_DIR because post-commit is executed in .git/hooks/ which is not in the repository
+  REPOSITORY_DIR=${1}
+else
+  REPOSITORY_DIR=$(git rev-parse --show-toplevel)  #  not called by post-commit
+  if [[ "${0}" != $(basename "${0}") ]] ; then  #  is script executed in TEST/<COMMIT_FILE_NAME>/ directory
+    cd $(dirname "${0}")
+  fi
+fi
 
 #    Uncomment shared TEST cases for TESTing
-ln -fs "${REPOSITORY_DIR}/hooks/EXAMPLES/FVT-option-help-001"          FVT-option-help-001
+#  ln -fs "${REPOSITORY_DIR}/hooks/EXAMPLES/FVT-option-help-001"          FVT-option-help-001
 #  ln -fs "${REPOSITORY_DIR}/hooks/EXAMPLES/FVT-option-help-002"          FVT-option-help-002
 #  ln -fs "${REPOSITORY_DIR}/hooks/EXAMPLES/FVT-option-help-003"          FVT-option-help-003
 #  ln -fs "${REPOSITORY_DIR}/hooks/EXAMPLES/FVT-option-help-004"          FVT-option-help-004
@@ -57,6 +60,8 @@ ln -fs "${REPOSITORY_DIR}/hooks/EXAMPLES/FVT-option-help-001"          FVT-optio
 #  ln -fs "${REPOSITORY_DIR}/hooks/EXAMPLES/FVT-option-version-001"       FVT-option-version-001
 #  ln -fs "${REPOSITORY_DIR}/hooks/EXAMPLES/FVT-option-version-002"       FVT-option-version-002
 #  ln -fs "${REPOSITORY_DIR}/hooks/EXAMPLES/FVT-option-version-003"       FVT-option-version-003
+#  ln -fs FVT-option-version-001.expected                                 FVT-option-version-002.expected
+#  ln -fs FVT-option-version-001.expected                                 FVT-option-version-003.expected
 #
 #  ln -fs "${REPOSITORY_DIR}/hooks/EXAMPLES/FVT-exit-code-error-124-001"  FVT-exit-code-error-124-001
 #  ln -fs "${REPOSITORY_DIR}/hooks/EXAMPLES/FVT-exit-code-error-124-002"  FVT-exit-code-error-124-002
