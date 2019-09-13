58791d6744dd1b7614da779c896a32fc09a00450
Bradley Allen <allen.bradley@ymail.com>
2019-09-13 10:47:49 -0500

    deleted:    check-git-TEST-cases.sh

--- hooks/bin/check-git-TEST-cases.sh
+++
@@ -1,2 +0,0 @@
-# 	hooks/bin/check-git-TEST-cases.sh  2.66.493  2019-09-13T10:23:49.967906-05:00 (CDT)  https://github.com/BradleyA/git-TEST-commit-automation.git  uadmin  five-rpi3b.cptx86.com 2.65-3-gf2f6e1a  
-# 	   rename check-git-TEST-cases.sh list-git-TEST-cases.sh 
