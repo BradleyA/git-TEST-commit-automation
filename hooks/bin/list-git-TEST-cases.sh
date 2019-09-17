3db91b5e7284466eaadb3d0b891601e3cfe02d52
Bradley Allen <allen.bradley@ymail.com>
2019-09-17 10:31:54 -0500

    removed list-git-TEST-cases.sh

--- hooks/bin/list-git-TEST-cases.sh
+++
@@ -1,2 +0,0 @@
-# 	hooks/bin/list-git-TEST-cases.sh  2.90.546  2019-09-17T10:27:47.475036-05:00 (CDT)  https://github.com/BradleyA/git-TEST-commit-automation.git  uadmin  five-rpi3b.cptx86.com 2.89  
-# 	   hooks/bin/list-git-TEST-cases.sh   rename to git-TEST-cases.sh 
