5d80edf57b3617a076b9be66cf09425ea34d88c3
Bradley Allen <allen.bradley@ymail.com>
2019-09-27 19:31:35 -0500

    testing

--- hooks/bin/git-TEST-cases.sh
+++ hooks/bin/git-TEST-cases.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# 	hooks/bin/git-TEST-cases.sh  2.146.797  2019-09-27T19:27:41.313323-05:00 (CDT)  https://github.com/BradleyA/git-TEST-commit-automation.git  uadmin  five-rpi3b.cptx86.com 2.145  
+# 	hooks/bin/git-TEST-cases.sh  2.147.798  2019-09-27T19:31:35.643318-05:00 (CDT)  https://github.com/BradleyA/git-TEST-commit-automation.git  uadmin  five-rpi3b.cptx86.com 2.146  
 # 	   testing 
 # 	hooks/bin/git-TEST-cases.sh  2.122.626  2019-09-21T15:39:47.409524-05:00 (CDT)  https://github.com/BradleyA/git-TEST-commit-automation.git  uadmin  five-rpi3b.cptx86.com 2.121  
 # 	   close #29   hooks/bin/git-TEST-cases.sh   add option --add - Add default test case directory (TEST/<FILE_NAME>/) and files SA-setup.sh, SA-cleanup.sh, SA-shellcheck-001.expected 
