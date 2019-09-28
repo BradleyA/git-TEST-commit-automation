a12c8789555c3225ef5f38b0094d8182e2f4fb62
Bradley Allen <allen.bradley@ymail.com>
2019-09-27 19:10:30 -0500

    test post-commit removing COMMIT_FILE_LIST if not DEBUG == 1

--- hooks/bin/git-TEST-cases.sh
+++ hooks/bin/git-TEST-cases.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# 	hooks/bin/git-TEST-cases.sh  2.142.791  2019-09-27T19:02:41.172567-05:00 (CDT)  https://github.com/BradleyA/git-TEST-commit-automation.git  uadmin  five-rpi3b.cptx86.com 2.141  
+# 	hooks/bin/git-TEST-cases.sh  2.143.792  2019-09-27T19:10:30.326664-05:00 (CDT)  https://github.com/BradleyA/git-TEST-commit-automation.git  uadmin  five-rpi3b.cptx86.com 2.142  
 # 	   test post-commit removing COMMIT_FILE_LIST if not DEBUG == 1 
 # 	hooks/bin/git-TEST-cases.sh  2.122.626  2019-09-21T15:39:47.409524-05:00 (CDT)  https://github.com/BradleyA/git-TEST-commit-automation.git  uadmin  five-rpi3b.cptx86.com 2.121  
 # 	   close #29   hooks/bin/git-TEST-cases.sh   add option --add - Add default test case directory (TEST/<FILE_NAME>/) and files SA-setup.sh, SA-cleanup.sh, SA-shellcheck-001.expected 
