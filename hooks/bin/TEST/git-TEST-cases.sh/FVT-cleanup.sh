8a933ad7b594f305eff8b22974586534ee0b6796
Bradley Allen <allen.bradley@ymail.com>
2020-01-20 12:55:39 -0600

    hooks/bin/TEST/git-TEST-cases.sh/FVT-setup.sh hooks/bin/TEST/git-TEST-cases.sh/FVT-cleanup.sh   copy FVT- scripts to customize

--- hooks/bin/TEST/git-TEST-cases.sh/FVT-cleanup.sh
+++ hooks/bin/TEST/git-TEST-cases.sh/FVT-cleanup.sh
@@ -1,34 +1,26 @@
 #!/bin/bash
-# 	hooks/bin/TEST/git-TEST-cases.sh/FVT-cleanup.sh  2.218.980  2020-01-16T21:19:52.945152-06:00 (CST)  https://github.com/BradleyA/git-TEST-commit-automation.git  dev  uadmin  five-rpi3b.cptx86.com 2.217  
-# 	   hooks/bin/TEST/git-TEST-cases.sh/FVT-cleanup.sh   Remove FVT-test-case.expected that has a size of zero  #36 
-# 	hooks/bin/TEST/git-TEST-cases.sh/FVT-cleanup.sh  2.217.979  2020-01-16T20:05:35.763558-06:00 (CST)  https://github.com/BradleyA/git-TEST-commit-automation.git  dev  uadmin  five-rpi3b.cptx86.com 2.216-3-g4aace7e  
-# 	   testing 
-# 	hooks/bin/TEST/git-TEST-cases.sh/FVT-cleanup.sh  2.202.881  2019-10-04T16:38:13.484777-05:00 (CDT)  https://github.com/BradleyA/git-TEST-commit-automation.git  uadmin  five-rpi3b.cptx86.com 2.201  
-# 	   hooks/bin/TEST/git-TEST-cases.sh/FVT-cleanup.sh   remove -v option for rm 
-# 	hooks/bin/TEST/git-TEST-cases.sh/FVT-cleanup.sh  2.133.779  2019-09-26T21:10:19.219920-05:00 (CDT)  https://github.com/BradleyA/git-TEST-commit-automation.git  uadmin  five-rpi3b.cptx86.com 2.132-1-g91c4dee  
-# 	   hooks/bin/TEST/git-TEST-cases.sh/FVT-cleanup.sh  rm FVT-option-c-002.expected FVT-option-c-001.expected 
-# 	hooks/bin/TEST/git-TEST-cases.sh/FVT-cleanup.sh  2.132.777  2019-09-26T20:27:59.431245-05:00 (CDT)  https://github.com/BradleyA/git-TEST-commit-automation.git  uadmin  five-rpi3b.cptx86.com 2.131-1-g2bc39b3  
-# 	   hooks/bin/TEST/git-TEST-cases.sh/FVT-cleanup.sh  first custom edit 
-# 	hooks/EXAMPLES/FVT-cleanup.sh  2.108.598  2019-09-19T16:47:48.371636-05:00 (CDT)  https://github.com/BradleyA/git-TEST-commit-automation.git  uadmin  five-rpi3b.cptx86.com 2.107-7-gae522d0  
-# 	   upgrade Version section 
-# 	hooks/EXAMPLES/FVT-cleanup.sh  2.92.550  2019-09-17T10:54:54.487436-05:00 (CDT)  https://github.com/BradleyA/git-TEST-commit-automation.git  uadmin  five-rpi3b.cptx86.com 2.91  
-# 	   close #3   hooks/bin/git-TEST-cases.sh 
-# 	hooks/EXAMPLES/FVT-cleanup.sh  2.81.523  2019-09-14T23:16:43.457843-05:00 (CDT)  https://github.com/BradleyA/git-TEST-commit-automation.git  uadmin  five-rpi3b.cptx86.com 2.80-1-g8745c24  
-# 	   hooks/EXAMPLES/FVT-cleanup.sh   push to add version 
-#86# FVT-cleanup.sh - test case cleanup
+# 	hooks/bin/TEST/git-TEST-cases.sh/FVT-cleanup.sh  2.269.1066  2020-01-20T12:55:38.659739-06:00 (CST)  https://github.com/BradleyA/git-TEST-commit-automation.git  dev  uadmin  five-rpi3b.cptx86.com 2.268  
+# 	   hooks/bin/TEST/git-TEST-cases.sh/FVT-setup.sh hooks/bin/TEST/git-TEST-cases.sh/FVT-cleanup.sh   copy FVT- scripts to customize 
+#86# hooks/bin/TEST/git-TEST-cases.sh/FVT-cleanup.sh  -  test case cleanup
 ###  Production standard 3.0 shellcheck
-###  Production standard 5.1.160 Copyright
-#    Copyright (c) 2019 Bradley Allen
-#    MIT License is in the online DOCUMENTATION, DOCUMENTATION URL defined below.
-###  Production standard 1.3.496 DEBUG variable
+###  Production standard 5.3.555 Copyright                                    # 3.555
+#    Copyright (c) 2020 Bradley Allen                                         # 3.555
+#    MIT License is online in the repository as a file named LICENSE          # 3.555
+###  Production standard 1.3.550 DEBUG variable                                             # 3.550
 #    Order of precedence: environment variable, default code
 if [[ "${DEBUG}" == ""  ]] ; then DEBUG="0" ; fi   # 0 = debug off, 1 = debug on, 'export DEBUG=1', 'unset DEBUG' to unset environment variable (bash)
 if [[ "${DEBUG}" == "2" ]] ; then set -x    ; fi   # Print trace of simple commands before they are executed
 if [[ "${DEBUG}" == "3" ]] ; then set -v    ; fi   # Print shell input lines as they are read
-if [[ "${DEBUG}" == "4" ]] ; then set -e    ; fi   # Exit command has a non-zero exit status
+if [[ "${DEBUG}" == "4" ]] ; then set -e    ; fi   # Exit immediately if non-zero exit status
+if [[ "${DEBUG}" == "5" ]] ; then set -e -o pipefail ; fi   # Exit immediately if non-zero exit status and exit if any command in a pipeline errors
 #
 BOLD=$(tput -Txterm bold)
 NORMAL=$(tput -Txterm sgr0)
+RED=$(tput    setaf 1)
+YELLOW=$(tput setaf 3)
+CYAN=$(tput   setaf 6)
+WHITE=$(tput  setaf 7)
+
 #    Date and time function ISO 8601
 get_date_stamp() {
   DATE_STAMP=$(date +%Y-%m-%dT%H:%M:%S.%6N%:z)
@@ -43,22 +35,29 @@ LOCALHOST=$(hostname -f)
 #    Assumptions for the next two lines of code:  The second line in this script includes the script path & name as the second item and
 #    the script version as the third item separated with space(s).  The tool I use is called 'markit'. See example line below:
 #       template/template.sh  3.517.783  2019-09-13T18:20:42.144356-05:00 (CDT)  https://github.com/BradleyA/user-files.git  uadmin  one-rpi3b.cptx86.com 3.516  
-SCRIPT_NAME=$(head -2 "${0}" | awk '{printf $2}')
+SCRIPT_NAME=$(head -2 "${0}" | awk '{printf $2}')  #  Different from ${COMMAND_NAME}=$(echo "${0}" | sed 's/^.*\///'), SCRIPT_NAME = includes Git repository directory and can be used any where in script (for dev, test teams)
 SCRIPT_VERSION=$(head -2 "${0}" | awk '{printf $3}')
 if [[ "${SCRIPT_NAME}" == "" ]] ; then SCRIPT_NAME="${0}" ; fi
 if [[ "${SCRIPT_VERSION}" == "" ]] ; then SCRIPT_VERSION="v?.?" ; fi
 
-#    UID and GID
-USER_ID=$(id -u)
+#    GID
 GROUP_ID=$(id -g)
 
-###  Production standard 2.3.512 log format (WHEN WHERE WHAT Version Line WHO UID:GID [TYPE] Message)
-new_message() {  #  $1="${SCRIPT_NAME}"  $2="${LINENO}"  $3="DEBUG INFO ERROR WARN"  $4="message"
+###  Production standard 2.3.529 log format (WHEN WHERE WHAT Version Line WHO UID:GID [TYPE] Message)
+new_message() {  #  $1="${LINENO}"  $2="DEBUG INFO ERROR WARN"  $3="message"
   get_date_stamp
-  echo -e "${NORMAL}${DATE_STAMP} ${LOCALHOST} ${1}[$$] ${SCRIPT_VERSION} ${2} ${USER} ${USER_ID}:${GROUP_ID} ${BOLD}[${3}]${NORMAL}  ${4}"
+  echo -e "${NORMAL}${DATE_STAMP} ${LOCALHOST} ${SCRIPT_NAME}[$$] ${SCRIPT_VERSION} ${1} ${USER} ${UID}:${GROUP_ID} ${BOLD}[${2}]${NORMAL}  ${3}"
 }
 
-if [[ "${DEBUG}" == "1" ]] ; then new_message "${SCRIPT_NAME}" "${LINENO}" "DEBUG" "  Started..." 1>&2 ; fi
+#    INFO
+if [[ "${DEBUG}" == "1" ]] ; then new_message "${LINENO}" "${YELLOW}INFO${WHITE}" "  Started..." 1>&2 ; fi
+
+if [[ ! -x ${1} ]] ; then  #  Command invoked does not exist or cannot execute
+  RETURN_CODE=126
+  new_message "${LINENO}" "${RED}ERROR${WHITE}" "  ${RETURN_CODE} - File, ${1}, does not exist or have execute permission" 1>&2
+  echo "${BOLD}${YELLOW}Test case --->${NORMAL} ${0} ${1} 126 - File, ${1}, does not exist or have execute permission - ${BOLD}${RED}ERROR${CYAN} - ERROR${NORMAL}"
+  exit "${RETURN_CODE}"
+fi
 
 ###  Place test case cleanup here 
 
@@ -73,19 +72,15 @@ rm -f FVT-*.test-case-output
 #	rm -f FVT-option-version-001.expected
 #	rm -f FVT-option-version-002.expected
 #	rm -f FVT-option-version-003.expected
-#	rm -f FVT-option-a-001.expected
-#	rm -f FVT-option-a-002.expected
-#	rm -f FVT-option-c-001.expected
-#	rm -f FVT-option-c-002.expected
 #    Remove linked FVT-* files except FVT-cleanup.sh and FVT-setup.sh
 for k in $(ls -1 FVT-*) ; do
   if [[ "${k}" != "FVT-cleanup.sh" ]] ; then
     if [[ "${k}" != "FVT-setup.sh" ]] ; then
       { [[ ! -L "${k}" ]] || rm "${k}"; }  #  Remove files with symbolic link
       if [[ ! -s "${k}" ]] ; then  #  Remove FVT-test-case.expected that has a size of zero
-        if [[ "${DEBUG}" == "1" ]] ; then new_message "${SCRIPT_NAME}" "${LINENO}" "DEBUG" "  Empty file ${k}" 1>&2 ; fi
+        if [[ "${DEBUG}" == "1" ]] ; then new_message "${LINENO}" "DEBUG" "  Empty file ${k}" 1>&2 ; fi
         if [[ "${k##*.}" == "expected" ]] ; then
-          if [[ "${DEBUG}" == "1" ]] ; then new_message "${SCRIPT_NAME}" "${LINENO}" "DEBUG" "  File ${k} has expected extension >${k##*.}<" 1>&2 ; fi
+          if [[ "${DEBUG}" == "1" ]] ; then new_message "${LINENO}" "DEBUG" "  File ${k} has expected extension >${k##*.}<" 1>&2 ; fi
           rm "${k}"
         fi
       fi
@@ -93,5 +88,5 @@ for k in $(ls -1 FVT-*) ; do
   fi
 done
 
-if [[ "${DEBUG}" == "1" ]] ; then new_message "${SCRIPT_NAME}" "${LINENO}" "INFO" "  Operation finished..." 1>&2 ; fi
+if [[ "${DEBUG}" == "1" ]] ; then new_message "${LINENO}" "${YELLOW}INFO${WHITE}" "  Operation finished..." 1>&2 ; fi
 ###
