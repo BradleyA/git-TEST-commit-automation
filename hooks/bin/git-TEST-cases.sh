42ca8004d86d614d2c76dc732d6d6f4858a355b5
Bradley Allen <allen.bradley@ymail.com>
2020-01-23 17:06:44 -0600

    testing --none

--- hooks/bin/git-TEST-cases.sh
+++ hooks/bin/git-TEST-cases.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# 	hooks/bin/git-TEST-cases.sh  2.302.1177  2020-01-23T17:03:34.140713-06:00 (CST)  https://github.com/BradleyA/git-TEST-commit-automation.git  master  uadmin  five-rpi3b.cptx86.com 2.301  
+# 	hooks/bin/git-TEST-cases.sh  2.303.1179  2020-01-23T17:06:44.310732-06:00 (CST)  https://github.com/BradleyA/git-TEST-commit-automation.git  master  uadmin  five-rpi3b.cptx86.com 2.302  
 # 	   testing --none 
 # 	hooks/bin/git-TEST-cases.sh  2.271.1069  2020-01-21T15:02:14.982224-06:00 (CST)  https://github.com/BradleyA/git-TEST-commit-automation.git  dev  uadmin  five-rpi3b.cptx86.com 2.270-1-g01cdcc2
 # 	   FVT-cleanup.sh   Production standard 5.3.559 Copyright and correct an incident found during testing 
