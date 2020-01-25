467e6f92ca867b3d913c55c9d405dd3125fea112
Bradley Allen <allen.bradley@ymail.com>
2020-01-25 15:51:07 -0600

    testing test case short names

--- hooks/bin/git-TEST-cases.sh
+++ hooks/bin/git-TEST-cases.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# 	hooks/bin/git-TEST-cases.sh  2.353.1240  2020-01-25T15:39:22.331839-06:00 (CST)  https://github.com/BradleyA/git-TEST-commit-automation.git  master  uadmin  five-rpi3b.cptx86.com 2.352  
+# 	hooks/bin/git-TEST-cases.sh  2.354.1241  2020-01-25T15:51:07.531759-06:00 (CST)  https://github.com/BradleyA/git-TEST-commit-automation.git  master  uadmin  five-rpi3b.cptx86.com 2.353  
 # 	   testing test case short names 
 # 	hooks/bin/git-TEST-cases.sh  2.342.1225  2020-01-24T20:59:53.478623-06:00 (CST)  https://github.com/BradleyA/git-TEST-commit-automation.git  master  uadmin  five-rpi3b.cptx86.com 2.341  
 # 	   hooks/bin/git-TEST-cases.sh   Production standard 9.3.562 Parse CLI options and arguments 
