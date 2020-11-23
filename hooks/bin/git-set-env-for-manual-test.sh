b8bd96cd607e5d2ef84385982f616e50be59bc66
Bradley Allen <allen.bradley@ymail.com>
2020-11-23 12:42:56 -0600

    hooks/bin/git-set-env-for-manual-test.sh -->   add code to support new_message

--- hooks/bin/git-set-env-for-manual-test.sh
+++ hooks/bin/git-set-env-for-manual-test.sh
@@ -1,25 +1,60 @@
 #!/bin/bash
-# 	hooks/bin/set-env-for-manual-test.sh  3.1.95.1657  2020-09-05T19:44:58.005025-05:00 (CDT)  https://github.com/BradleyA/git-TEST-commit-automation.git  master  uadmin  five-rpi3b.cptx86.com 3.1.94-3-g673ec73  
-# 	   hooks/bin/set-env-for-manual-test.sh -->   add No --help  
-# 	hooks/bin/set-env-for-manual-test.sh  3.1.93.1652  2020-09-05T07:57:20.901435-05:00 (CDT)  https://github.com/BradleyA/git-TEST-commit-automation.git  master  uadmin  five-rpi3b.cptx86.com 3.1.92-1-g500781d  
-# 	   hooks/bin/set-env-for-manual-test.sh -->   correct typo in help comment  
-#86# hooks/bin/set-env-for-manual-test.sh
-#    Run this script when manually testing test cases    . hooks/bin/set-env-for-manual-test.sh  OR    source hooks/bin/set-env-for-manual-test.sh
+# 	hooks/bin/git-set-env-for-manual-test.sh  3.1.145.1871  2020-11-23T12:42:56.356203-06:00 (CST)  https://github.com/BradleyA/git-TEST-commit-automation.git  master  uadmin  five-rpi3b.cptx86.com 3.1.144  
+# 	   hooks/bin/git-set-env-for-manual-test.sh -->   add code to support new_message  
+#86# hooks/bin/git-set-env-for-manual-test.sh
+#    Run this script when manually testing test cases    . hooks/bin/git-set-env-for-manual-test.sh  OR  source hooks/bin/git-set-env-for-manual-test.sh
 #    This script sets REPOSITORY_DIR and REPOSITORY_DIR_COUNT environment variables which are normally set when post-commit is run
 #    This is required because post-commit is has not run before manually testing a test case
 ###    
+#
+BOLD=$(tput -Txterm bold)
+UNDERLINE=$(tput -Txterm sgr 0 1)  # 0.3.583
+NORMAL=$(tput -Txterm sgr0)
+RED=$(tput    setaf 1)
+YELLOW=$(tput setaf 3)
+BLUE=$(tput   setaf 4)
+PURPLE=$(tput setaf 5)
+WHITE=$(tput  setaf 7)
 
-echo -e "\nRun this script before manually testing git-TEST-commit-automation test cases:\n\n\tsource  hooks/bin/set-env-for-manual-test.sh\n"
-echo    "This script sets REPOSITORY_DIR and REPOSITORY_DIR_COUNT environment variables"
-echo    "which are normally set when post-commit is run.  This is required because"
-echo    "post-commit has not run before manually testing a test case."
+#    Date and time function ISO 8601
+get_date_stamp() {
+  DATE_STAMP=$(date +%Y-%m-%dT%H:%M:%S.%6N%:z)
+  TEMP=$(date +%Z)
+  DATE_STAMP="${DATE_STAMP} (${TEMP})"
+}
+
+#    Fully qualified domain name FQDN hostname
+LOCALHOST=$(hostname -f)
+
+#    Version
+#    Assumptions for the next two lines of code:  The second line in this script includes the script path & name as the second item and
+#    the script version as the third item separated with space(s).  The tool I use is called 'markit'. See example line below:
+#       template/template.sh  3.517.783  2019-09-13T18:20:42.144356-05:00 (CDT)  https://github.com/BradleyA/user-files.git  uadmin  one-rpi3b.cptx86.com 3.516  
+SCRIPT_NAME=$(head -2 "${0}" | awk '{printf $2}')  #  Different from ${COMMAND_NAME}=$(echo "${0}" | sed 's/^.*\///'), SCRIPT_NAME = includes Git repository directory and can be used anywhere in script (for dev, test teams)
+SCRIPT_VERSION=$(head -2 "${0}" | awk '{printf $3}')
+if [[ "${SCRIPT_NAME}" == "" ]] ; then SCRIPT_NAME="${0}" ; fi
+if [[ "${SCRIPT_VERSION}" == "" ]] ; then SCRIPT_VERSION="v?.?" ; fi
+
+#    GID
+GROUP_ID=$(id -g)
+
+###  Production standard 2.3.578 Log format (WHEN WHERE WHAT Version Line WHO UID:GID [TYPE] Message)
+new_message() {  #  $1="${LINENO}"  $2="DEBUG INFO ERROR WARN"  $3="message"
+  get_date_stamp
+  echo -e "${NORMAL}${DATE_STAMP} ${LOCALHOST} ${SCRIPT_NAME}[$$] ${BOLD}${BLUE}${SCRIPT_VERSION} ${PURPLE}${1}${NORMAL} ${USER} ${UID}:${GROUP_ID} ${BOLD}[${2}]${NORMAL}  ${3}"
+}
 
 #    No --help  # 0.3.583
 if [[ "${1}" != "" ]] ; then
   new_message "${LINENO}" "${RED}ERROR${WHITE}" "  ${SCRIPT_NAME} does not support arguements." 1>&2
-  echo -e "    For more information:\n${BOLD}${YELLOW}    ${UNDERLINE}https://github.com/BradleyA/git-TEST-commit-automation/tree/master/hooks#git-test-commit-automation------\n${NORMAL}"  # 0.3.583
+  echo -e "    For more information:\n${BOLD}${YELLOW}    ${UNDERLINE}https://github.com/BradleyA/git-TEST-commit-automation/blob/master/hooks/README.md\n${NORMAL}"  # 0.3.583
   exit 1
 fi
+
+echo -e "\nRun this script before manually testing git-TEST-commit-automation test cases:\n\n\t${BOLD}${YELLOW}source  hooks/bin/git-set-env-for-manual-test.sh${NORMAL}\n"
+echo    "This script sets ${BOLD}REPOSITORY_DIR${NORMAL} and ${BOLD}REPOSITORY_DIR_COUNT${NORMAL} environment variables"
+echo    "which are normally set when post-commit is run.  This is required because"
+echo    "post-commit has not run before manually testing a test case."
 
 REPOSITORY_DIR=$(git rev-parse --show-toplevel)
 export REPOSITORY_DIR  #  export REPOSITORY_DIR so test scripts can use it
