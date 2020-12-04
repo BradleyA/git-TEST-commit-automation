411370b795f74938d9ec7546091b5183d929d40d
Bradley Allen <allen.bradley@ymail.com>
2020-12-04 09:39:50 -0600

    hooks/bob/file-jim.sh hooks/bob/file-sally.sh -->   testing

--- hooks/bob/file-jim.sh
+++ hooks/bob/file-jim.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
+# 	hooks/bob/file-jim.sh  3.1.253.2002  2020-12-04T09:39:50.296864-06:00 (CST)  https://github.com/BradleyA/git-TEST-commit-automation.git  master  uadmin  five-rpi3b.cptx86.com 3.1.252  
+# 	   hooks/bob/file-jim.sh hooks/bob/file-sally.sh -->   testing  
 # 	hooks/bob/file-jim.sh  3.1.252.2001  2020-12-04T09:37:18.199591-06:00 (CST)  https://github.com/BradleyA/git-TEST-commit-automation.git  master  uadmin  five-rpi3b.cptx86.com 3.1.251  
 # 	   hooks/bob/file-jim.sh hooks/bob/file-sally.sh -->   testing  
 # 	hooks/bob/file-jim.sh  3.1.251.2000  2020-12-04T09:34:03.360686-06:00 (CST)  https://github.com/BradleyA/git-TEST-commit-automation.git  master  uadmin  five-rpi3b.cptx86.com 3.1.250-1-g2013d8a  
 # 	   hooks/bob/file-jim.sh hooks/bob/file-sally.sh -->   testing  
-# 	hooks/bob/file-jim.sh  3.1.250.1999  2020-12-04T09:26:02.021454-06:00 (CST)  https://github.com/BradleyA/git-TEST-commit-automation.git  master  uadmin  five-rpi3b.cptx86.com 3.1.250  
-# 	   hooks/bob/file-jim.sh -->   testing  
 #
 
 echo "${0}"
--- hooks/bob/file-sally.sh
+++ hooks/bob/file-sally.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# 	hooks/bob/file-sally.sh  3.1.252.2001  2020-12-04T09:37:18.475392-06:00 (CST)  https://github.com/BradleyA/git-TEST-commit-automation.git  master  uadmin  five-rpi3b.cptx86.com 3.1.251  
+# 	hooks/bob/file-sally.sh  3.1.253.2002  2020-12-04T09:39:50.567009-06:00 (CST)  https://github.com/BradleyA/git-TEST-commit-automation.git  master  uadmin  five-rpi3b.cptx86.com 3.1.252  
 # 	   hooks/bob/file-jim.sh hooks/bob/file-sally.sh -->   testing  
-# 	hooks/bob/file-sally.sh  3.1.251.2000  2020-12-04T09:34:03.604942-06:00 (CST)  https://github.com/BradleyA/git-TEST-commit-automation.git  master  uadmin  five-rpi3b.cptx86.com 3.1.250-1-g2013d8a  
+# 	hooks/bob/file-sally.sh  3.1.252.2001  2020-12-04T09:37:18.475392-06:00 (CST)  https://github.com/BradleyA/git-TEST-commit-automation.git  master  uadmin  five-rpi3b.cptx86.com 3.1.251  
 # 	   hooks/bob/file-jim.sh hooks/bob/file-sally.sh -->   testing  
 #
 echo 'testing'
