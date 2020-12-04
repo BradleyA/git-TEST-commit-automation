98294cd40677936e43efa919a7f8f04884f1c16e
Bradley Allen <allen.bradley@ymail.com>
2020-12-04 00:28:32 -0600

    hooks/bob/TEST/file-bob.sh/SA-shellcheck-001.expected hooks/bob/file-bob.sh -->   testing

--- hooks/bob/file-bob.sh
+++ hooks/bob/file-bob.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# 	hooks/bob/file-bob.sh  3.1.247.1995  2020-12-04T00:28:32.545781-06:00 (CST)  https://github.com/BradleyA/git-TEST-commit-automation.git  master  uadmin  five-rpi3b.cptx86.com 3.1.246  
+# 	   hooks/bob/TEST/file-bob.sh/SA-shellcheck-001.expected hooks/bob/file-bob.sh -->   testing  
 # 	hooks/bob/file-bob.sh  3.1.246.1994  2020-12-04T00:27:52.656099-06:00 (CST)  https://github.com/BradleyA/git-TEST-commit-automation.git  master  uadmin  five-rpi3b.cptx86.com 3.1.245  
 # 	   hooks/bob/file-bob.sh -->   testing  
 # 	hooks/bob/file-bob.sh  3.1.245.1993  2020-12-04T00:23:37.391290-06:00 (CST)  https://github.com/BradleyA/git-TEST-commit-automation.git  master  uadmin  five-rpi3b.cptx86.com 3.1.244  
@@ -9,5 +11,3 @@
 # 	hooks/bob/file-bob.sh  3.1.242.1990  2020-12-04T00:06:44.494003-06:00 (CST)  https://github.com/BradleyA/git-TEST-commit-automation.git  master  uadmin  five-rpi3b.cptx86.com 3.1.241  
 # 	   hooks/bob/file-bob.sh -->   testing  
 # testing
-
-echo "hello world"
