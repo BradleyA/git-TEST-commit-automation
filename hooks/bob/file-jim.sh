2013d8a1146f6618a9275d0e4c43f233d544483c
Bradley Allen <allen.bradley@ymail.com>
2020-12-04 09:26:02 -0600

    hooks/bob/file-jim.sh -->   testing

---
+++ hooks/bob/file-jim.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# 	hooks/bob/file-jim.sh  3.1.250.1999  2020-12-04T09:26:02.021454-06:00 (CST)  https://github.com/BradleyA/git-TEST-commit-automation.git  master  uadmin  five-rpi3b.cptx86.com 3.1.250  
+# 	   hooks/bob/file-jim.sh -->   testing  
+#
+
+echo ${0}
+
