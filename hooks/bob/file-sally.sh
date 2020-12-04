30cdee237628e73327826b25c7da0a1200ee4d23
Bradley Allen <allen.bradley@ymail.com>
2020-12-04 09:37:18 -0600

    hooks/bob/file-jim.sh hooks/bob/file-sally.sh -->   testing

--- hooks/bob/file-sally.sh
+++ hooks/bob/file-sally.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# 	hooks/bob/file-sally.sh  3.1.252.2001  2020-12-04T09:37:18.475392-06:00 (CST)  https://github.com/BradleyA/git-TEST-commit-automation.git  master  uadmin  five-rpi3b.cptx86.com 3.1.251  
+# 	   hooks/bob/file-jim.sh hooks/bob/file-sally.sh -->   testing  
 # 	hooks/bob/file-sally.sh  3.1.251.2000  2020-12-04T09:34:03.604942-06:00 (CST)  https://github.com/BradleyA/git-TEST-commit-automation.git  master  uadmin  five-rpi3b.cptx86.com 3.1.250-1-g2013d8a  
 # 	   hooks/bob/file-jim.sh hooks/bob/file-sally.sh -->   testing  
 #
