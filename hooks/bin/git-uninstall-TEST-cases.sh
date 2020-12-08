73674b00e1e2179528fb6d6152435a8e9a57f30b
Bradley Allen <allen.bradley@ymail.com>
2020-12-08 12:09:58 -0600

    hooks/bin/git-uninstall-TEST-cases.sh hooks/post-commit -->   update comments

--- hooks/bin/git-uninstall-TEST-cases.sh
+++ hooks/bin/git-uninstall-TEST-cases.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# 	hooks/bin/git-uninstall-TEST-cases.sh  3.1.286.2058  2020-12-08T12:09:55.890646-06:00 (CST)  https://github.com/BradleyA/git-TEST-commit-automation.git  master  uadmin  five-rpi3b.cptx86.com 3.1.285  
+# 	   hooks/bin/git-uninstall-TEST-cases.sh hooks/post-commit -->   update comments  
 # 	hooks/bin/uninstall-git-TEST-cases.sh  3.1.126.1834  2020-11-18T13:46:55.038582-06:00 (CST)  https://github.com/BradleyA/git-TEST-commit-automation.git  master  uadmin  five-rpi3b.cptx86.com 3.1.125  
 # 	   hooks/bin/git-TEST-cases.sh hooks/bin/git-setup-TEST-cases.sh hooks/bin/uninstall-git-TEST-cases.sh -->   rename git-TEST-setup-cases.sh -> git-setup-TEST-cases.sh  
 # 	hooks/bin/uninstall-git-TEST-cases.sh  2.443.1337  2020-01-29T11:21:10.587150-06:00 (CST)  https://github.com/BradleyA/git-TEST-commit-automation.git  master  uadmin  five-rpi3b.cptx86.com 2.442-2-g61001a8  
@@ -115,14 +117,14 @@ if git -C . rev-parse 2> /dev/null ; then  #  currect directory in a git reposit
   #
   rm -f  .git/hooks/pre-commit
   rm -f  .git/hooks/post-commit
+  # >>>  need a ticket to add an options for git push with environment variable
+  # >>>  git push
+  # >>>  add command for testing manually need to set enviroment variables first
   rm -f  /usr/local/bin/git-TEST-cases.sh
   rm -f  /usr/local/bin/git-setup-TEST-cases.sh
   rm -f  /usr/local/bin/uninstall-git-TEST-cases.sh
   mv "${TMP_FILE_1}.tar" .  #  #20   place the tar file in the repository top directory not /tmp
   git commit -m 'remove git-TEST-commit-automation  hooks recursively'
-  #>>>  need a ticket to add an options for git push with environment variable
-  #>>>  git push
-  #
 else
   EXIT_CODE=${?}
   new_message "${LINENO}" "${RED}ERROR${WHITE}" "  The current directory, ($(pwd)), is Not a git repository (or any of the parent directories)." 1>&2
